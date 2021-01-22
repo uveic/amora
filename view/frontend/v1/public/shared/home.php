e551d97aa33316c576a273c4c4a39587a4fec336
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-01-22 12:37:56 +0100

    Minor homepage changes

--- view/frontend/v1/public/shared/home.php
+++ view/frontend/v1/public/shared/home.php
@@ -10,10 +10,10 @@ use uve\core\model\response\HtmlResponseData;
 <?= $this->insert('shared/partials/head', ['responseData' => $responseData]) ?>
 <body>
 <section class="content-medium-width m-t-6 m-b-6">
-  <h1>Hi, I'm Victor 👋<br> I'm a software developer living in Lugo, Spain.</h1>
-  <h3>This is my personal site. It is a work in progress. Soon(-ish) you will find here my blog and personal projects.</h3>
-  <h3>Please feel free to say hi anytime.</h3>
-  <h3>Have a great day!</h3>
+  <h1 class="small">Hi, I'm Victor 👋<br> I'm a software developer living in Lugo, Spain.</h1>
+  <p>This is my personal site. It is a work in progress. Soon(-ish) you will find here my blog and personal projects.</p>
+  <p>Please feel free to say hi anytime.</p>
+  <p>Have a great day!</p>
   <div id="home-links" class="m-t-3">
     <a target="_blank" href="mailto:victor@victorgonzalez.eu">victor@victorgonzalez.eu</a>
     <a target="_blank" href="https://twitter.com/uveic">Twitter</a>
