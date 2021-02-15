0bc08e9e9a440b6152d80988c8c60c4ee9405b4d
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-02-15 19:30:12 +0100

    Move codegen inside core/bin

--- core/bin/codegen/index.js
+++ core/bin/codegen/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const util = require('./lib/util');
 
-const controllerFolderPath = __dirname + '/../controller/';
+const controllerFolderPath = __dirname + '/../../../router/controller/';
 const phpControllerTemplate = require('./class.php');
 const phpResponsesTemplate = require('./responses.php');
 
