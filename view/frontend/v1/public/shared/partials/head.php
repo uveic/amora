0526ca69b9292c64e9473d4d1fb9bbe6ee6d1704
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-03-15 11:09:51 +0100

    Add blackberry favicon files

--- view/frontend/v1/public/shared/partials/head.php
+++ view/frontend/v1/public/shared/partials/head.php
@@ -27,6 +27,7 @@ use Amora\Core\Model\Response\HtmlResponseDataAbstract;
   <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/img/favicon/site.webmanifest">
   <link rel="alternate" href="<?=$responseData->getSiteUrl()?>" hreflang="x-default">
   <link href="/css/style.css" rel="stylesheet" type="text/css">
 </head>
