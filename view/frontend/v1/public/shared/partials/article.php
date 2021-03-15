f6defe8f6cac558b71de80240f9762cae4c76ee5
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-03-15 11:03:56 +0100

    Do not display edit button when previewing an article

--- view/frontend/v1/public/shared/partials/article.php
+++ view/frontend/v1/public/shared/partials/article.php
@@ -6,11 +6,12 @@ use Amora\Core\Module\Article\Value\ArticleType;
 /** @var HtmlResponseData $responseData */
 $article = $responseData->getFirstArticle();
 $canEdit = $responseData->getSession() && $responseData->getSession()->isAdmin();
+$preview = $responseData->getRequest()->getGetParam('preview');
 
 if ($article) {
 ?>
   <article>
-<?php if ($canEdit) { ?>
+<?php if ($canEdit && !$preview) { ?>
     <a class="article-edit" href="<?=$responseData->getBaseUrlWithLanguage()?>backoffice/articles/<?=$article->getId()?>"><?=strtolower($responseData->getLocalValue('globalEdit'))?></a>
 <?php } ?>
     <?=$article->getContentHtml()?>
