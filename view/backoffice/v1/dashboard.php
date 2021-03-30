0332e3edea5027ae8356b7af7e3e28341f8d5119
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-03-30 22:36:06 +0200

    CSS

--- view/backoffice/v1/dashboard.php
+++ view/backoffice/v1/dashboard.php
@@ -7,9 +7,11 @@ use Amora\Core\Model\Response\HtmlResponseDataAuthorised;
 $this->layout('base', ['responseData' => $responseData])
 ?>
   <main>
-    <section class="content">
+    <section>
       <div id="feedback" class="feedback null"></div>
-      <h1 class="h1-flex"><?=$this->e($responseData->getLocalValue('navAdminDashboard'))?></h1>
+      <div class="m-r-1 m-l-1">
+        <h1><?=$this->e($responseData->getLocalValue('navAdminDashboard'))?></h1>
+      </div>
       <div class="content-flex">
         <?=$this->insert('partials/dashboard/links', ['responseData' => $responseData])?>
         <?=$this->insert('partials/dashboard/homepage', ['responseData' => $responseData])?>
