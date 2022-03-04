7b8add121bba3443390d4d3046cdd5cfcd32f6dc
Victor Gonzalez <victor.gonzalez@protonmail.com>
2022-03-04 10:44:28 +0100

    Language in articles: HTML and JS

--- view/backoffice/v1/partials/users-edit/control-bar.php
+++ view/backoffice/v1/partials/users-edit/control-bar.php
@@ -44,7 +44,6 @@ if ($userToEdit) {
 }
 
 $isEnabled = $userToEdit ? $userToEdit->isEnabled : true;
-$random = StringUtil::getRandomString(5);
 
 ?>
       <div class="control-bar-wrapper">
@@ -52,13 +51,13 @@ $random = StringUtil::getRandomString(5);
           <input type="submit" class="button button-form" value="<?=$userToEdit ? $responseData->getLocalValue('globalUpdate') : $responseData->getLocalValue('globalSave')?>">
         </div>
         <div class="control-bar-creation"><span><?=$updatedAtContent?></span><br><span><?=$createdAtContent?></span></div>
-        <input type="checkbox" id="user-status-dd-<?=$random?>" class="dropdown-menu">
-        <div class="dropdown-container">
+        <input type="checkbox" id="user-status-dd-checkbox" class="dropdown-menu">
+        <div class="dropdown-container user-status-container">
           <ul>
-            <li><a data-checked="<?=$isEnabled ? '1' : '0'?>" data-value="1" class="dropdown-menu-option user-enabled-option feedback-success" href="#"><?=$responseData->getLocalValue('globalActivated')?></a></li>
-            <li><a data-checked="<?=$isEnabled ? '0' : '1'?>" data-value="0" class="dropdown-menu-option user-enabled-option background-light-color" href="#"><?=$responseData->getLocalValue('globalDeactivated')?></a></li>
+            <li><a data-checked="<?=$isEnabled ? '1' : '0'?>" data-value="1" class="dropdown-menu-option user-status-dd-option feedback-success" href="#"><?=$responseData->getLocalValue('globalActivated')?></a></li>
+            <li><a data-checked="<?=$isEnabled ? '0' : '1'?>" data-value="0" class="dropdown-menu-option user-status-dd-option background-light-color" href="#"><?=$responseData->getLocalValue('globalDeactivated')?></a></li>
           </ul>
-          <label for="user-status-dd-<?=$random?>" class="dropdown-menu-label user-status-dd-label <?=$isEnabled ? 'feedback-success' : 'background-light-color' ?>">
+          <label id="user-status-dd-label" for="user-status-dd-checkbox" class="dropdown-menu-label <?=$isEnabled ? 'feedback-success' : 'background-light-color' ?>">
             <span><?=($isEnabled ? $responseData->getLocalValue('globalActivated') : $responseData->getLocalValue('globalDeactivated'))?></span>
             <img class="img-svg no-margin" width="20" height="20" src="/img/svg/caret-down.svg" alt="Change">
           </label>
