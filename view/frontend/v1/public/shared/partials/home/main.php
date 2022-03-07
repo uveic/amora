c00b2b86079b00dc90c77f2521a475e2961c6124
Victor Gonzalez <victor.gonzalez@protonmail.com>
2022-03-07 17:08:40 +0100

    Fix error: call isAdmin() on null

--- view/frontend/v1/public/shared/partials/home/main.php
+++ view/frontend/v1/public/shared/partials/home/main.php
@@ -6,7 +6,7 @@ use Amora\Core\Util\UrlBuilderUtil;
 /** @var HtmlHomepageResponseData $responseData */
 
 $homepageContent = $responseData->homepageContent;
-$editLink = $homepageContent && $responseData->request->session->isAdmin()
+$editLink = $homepageContent && $responseData->request->session && $responseData->request->session->isAdmin()
     ? '<p class="no-margin"><a href="' . UrlBuilderUtil::buildBackofficeArticleUrl($homepageContent->language, $homepageContent->id) . '">' . strtolower($responseData->getLocalValue('globalEdit')) . '</a></p>'
     : '';
 ?>
