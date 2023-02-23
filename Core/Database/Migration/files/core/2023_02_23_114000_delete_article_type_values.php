be92d51a416aba47307d46383ac029b5b09e0e3f
Victor Gonzalez <victor.gonzalez@protonmail.com>
2023-02-23 21:43:14 +0100

    Fix migration file

--- Core/Database/Migration/files/core/2023_02_23_114000_delete_article_type_values.php
+++ Core/Database/Migration/files/core/2023_02_23_114000_delete_article_type_values.php
@@ -6,6 +6,8 @@
 return "
     DELETE FROM core_content_history WHERE 1;
     DELETE FROM core_content WHERE 1;
+
+    INSERT INTO `core_content_type` (`id`, `name`) VALUES(1, 'Homepage'), (2, 'BlogBottom');
     
     INSERT INTO core_content (`language_iso_code`, `user_id`, `type_id`, `created_at`, `updated_at`, `title`, `subtitle`, `html`, `main_image_id`)
     SELECT
@@ -39,6 +41,14 @@ return "
     DELETE cas FROM core_article_section AS cas
         INNER JOIN core_article AS ca ON ca.id = cas.article_id
     WHERE ca.type_id IN (1, 100);
+
+    ALTER TABLE `core_article_history` DROP CONSTRAINT `article_history_article_id_fk`;
+    ALTER TABLE `core_article` DROP CONSTRAINT `article_type_id_fk`;
+
+    DELETE ap FROM core_article AS a
+        INNER JOIN core_article_path AS ap ON ap.article_id = a.id
+    WHERE a.type_id IN (1, 100);
+
     DELETE FROM core_article WHERE type_id IN (1, 100);
     DELETE FROM core_article_type WHERE id IN (1, 100);
 ";
