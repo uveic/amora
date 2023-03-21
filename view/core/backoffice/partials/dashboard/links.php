0a82c88bc5eaecfd984a04ab42b321ecee717f02
Víctor González <victor@victorgonzalez.eu>
2023-03-21 10:53:05 +0100

    CSS

--- view/core/backoffice/partials/dashboard/links.php
+++ view/core/backoffice/partials/dashboard/links.php
@@ -12,35 +12,35 @@ use Amora\Core\Util\UrlBuilderUtil;
           <div class="dashboard-cards-wrapper">
             <a href="<?=UrlBuilderUtil::buildBackofficeImagesUrl($responseData->siteLanguage)?>">
               <span class="value"><?=$responseData->dashboardCount->images?></span>
-              <span class="m-t-025">
+              <span>
                 <img class="img-svg img-svg-20 m-r-05" width="20" height="20" src="/img/svg/image.svg" alt="<?=$responseData->getLocalValue('navAdminImages')?>">
                 <?=$this->e($responseData->getLocalValue('navAdminImages'))?>
               </span>
             </a>
             <a href="<?=UrlBuilderUtil::buildBackofficeMediaUrl($responseData->siteLanguage)?>">
               <span class="value"><?=$responseData->dashboardCount->files?></span>
-              <span class="m-t-025">
+              <span>
                 <img class="img-svg img-svg-20 m-r-05" width="20" height="20" src="/img/svg/files.svg" alt="<?=$responseData->getLocalValue('navAdminMedia')?>">
                 <?=$this->e($responseData->getLocalValue('navAdminMedia'))?>
               </span>
             </a>
             <a href="<?=UrlBuilderUtil::buildBackofficeArticlesUrl($responseData->siteLanguage, ArticleType::Page)?>">
               <span class="value"><?=$responseData->dashboardCount->pages?></span>
-              <span class="m-t-025">
+              <span>
                 <img class="img-svg img-svg-20 m-r-05" width="20" height="20" src="/img/svg/note-pencil.svg" alt="<?=$responseData->getLocalValue('navAdminArticles')?>">
                 <?=$responseData->getLocalValue('navAdminArticles')?>
               </span>
             </a>
             <a href="<?=UrlBuilderUtil::buildBackofficeArticlesUrl($responseData->siteLanguage, ArticleType::Blog)?>">
               <span class="value"><?=$responseData->dashboardCount->blogPosts?></span>
-              <span class="m-t-025">
+              <span>
                 <img class="img-svg img-svg-20 m-r-05" width="20" height="20" src="/img/svg/article-medium.svg" alt="<?=$responseData->getLocalValue('navAdminBlogPosts')?>">
                 <?=$responseData->getLocalValue('navAdminBlogPosts')?>
               </span>
             </a>
             <a href="<?=UrlBuilderUtil::buildBackofficeUsersUrl($responseData->siteLanguage)?>">
               <span class="value"><?=$responseData->dashboardCount->users?></span>
-              <span class="m-t-025">
+              <span>
                 <img class="img-svg img-svg-20 m-r-05" width="20" height="20" src="/img/svg/users.svg" alt="<?=$responseData->getLocalValue('navAdminUsers')?>">
                 <?=$this->e($responseData->getLocalValue('navAdminUsers'))?>
               </span>
