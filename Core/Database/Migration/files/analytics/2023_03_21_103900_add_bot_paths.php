7d06ed11e0ba9b0bace42f432ba1ec5bc87dd7a8
Víctor González <victor@victorgonzalez.eu>
2023-03-21 10:47:44 +0100

    Add bot paths to database

---
+++ Core/Database/Migration/files/analytics/2023_03_21_103900_add_bot_paths.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Return SQL statement as a string
+ */
+
+return "
+    INSERT INTO `bot_path` (`path`)
+    VALUES
+        ('wp-includes/wlwmanifest.xml'),    
+        ('fckeditor/editor/filemanager/connectors/test.html'),
+        ('license.txt'),
+        ('humans.txt'),
+        ('vendor/laravel/.env'),
+        ('.ftpconfig'),
+        ('.well-known/autoconfig/mail/config-v1.1.xml'),
+        ('login/img/nyyh/chkjs.js'),
+        ('sites/all/libraries/fckeditor/editor/filemanager/connectors/test.html'),
+        ('sites/all/modules/fckeditor/fckeditor/editor/filemanager/connectors/test.html'),
+        ('admin/fckeditor/editor/filemanager/connectors/test.html'),
+        ('include/fckeditor/editor/filemanager/connectors/test.html'),
+        ('js/fckeditor/editor/filemanager/connectors/test.html'),
+        ('wp-content/plugins/formcraft/file-upload/server/php'),
+        ('abruzi.php4'),
+        ('wp-json/wp_live_chat_support/v1/remote_upload'),
+        ('old.zip'),
+        ('archive.zip'),
+        ('backup.zip'),
+        ('adminer-4.7.1'),
+        ('adminer-4.7.7'),
+        ('adminer-4.7.4'),
+        ('adminer-4.8.0'),
+        ('adminer-4.6.2'),
+        ('adminer-4.7.8'),
+        ('adminer-4.6.3'),
+        ('site.zip'),
+        ('www.zip'),
+        ('html.zip'),
+        ('sftp-config.json'),
+        ('.remote'),
+        ('.local'),
+        ('.production'),
+        ('Datavase/.env'),
+        ('autodiscover/autodiscover.xml'),
+        ('.well-known/carddav'),
+        ('.well-known/caldav'),
+        ('.vscode/sftp.json');
+";
