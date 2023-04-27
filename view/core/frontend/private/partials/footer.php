0fe99e97ccddffd8bc9492329b32469dd25bef36
Víctor González <victor@victorgonzalez.eu>
2023-04-27 13:04:40 +0200

    Bump up JS version

--- view/core/frontend/private/partials/footer.php
+++ view/core/frontend/private/partials/footer.php
@@ -1,3 +1,3 @@
   <script src="/js/lib/medium-editor.min.js"></script>
   <script type="module" src="/js/authorised-002.js"></script>
-  <script type="module" src="/js/backoffice-011.js"></script>
+  <script type="module" src="/js/backoffice-012.js"></script>
