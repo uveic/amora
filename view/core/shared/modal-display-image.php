9b21aa5f1e3e6f0dc2908ec9389d40d1f9600cd6
Víctor González <victor@victorgonzalez.eu>
2026-01-15 18:43:51 +0100

    From other project

--- view/core/shared/modal-display-image.php
+++ view/core/shared/modal-display-image.php
@@ -3,7 +3,7 @@
 use Amora\Core\Value\CoreIcons;
 
 ?>
-  <div class="modal-display-image modal-wrapper modal-media null">
-    <a href="#" class="modal-close-button modal-media-close"><?=CoreIcons::CLOSE?></a>
-    <div class="modal-inner"></div>
-  </div>
+    <div class="modal-display-image modal-wrapper modal-media null">
+      <a href="#" class="modal-close-button modal-media-close"><?=CoreIcons::CLOSE?></a>
+      <div class="modal-inner"></div>
+    </div>
